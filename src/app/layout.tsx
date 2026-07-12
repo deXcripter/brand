ea8ed227e732bd575dedf483a58e099da3f9912a
Johnpaul N. <dexcripter@gmail.com>
2026-07-12 09:23:21 +0100

    cp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,8 @@ export default function RootLayout({
           rel="stylesheet"
         />
 
-          <Script src="https://cloud.umami.is/script.js" data-website-id="7c2c13ef-0008-45b0-ac24-9be025ce80ba" />
+
+
 
 
         <Script
